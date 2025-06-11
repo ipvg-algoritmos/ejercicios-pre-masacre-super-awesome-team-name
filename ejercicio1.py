cfd608b251218762bb13162dcad5e51699ddd457
Oscarriel <oscar.carriel@virginiogomez.cl>
2025-06-11 02:59:25 +0000

    Ejercicio 1 completo real

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,9 @@
 lista = [1,2,3,4,5,6,7,8,9,10]
+num = int(input("Seleccione el numero que desea buscar: "))
+
+for i in lista:
+    if i == num:
+        print(f"El numero se encuentra en la lista y su posicion es numero es: {i}")
+    else:
+        print(f"El numero {num} no existe en la lista")
+        break
