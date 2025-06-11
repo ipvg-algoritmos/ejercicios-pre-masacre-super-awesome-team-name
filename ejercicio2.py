c235d8524a9693c639b8ed34fa1d03c7dcbc7344
Oscarriel <oscar.carriel@virginiogomez.cl>
2025-06-11 03:48:03 +0000

    Ejercicio2 Revisado

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,4 @@
-frase= input("crea una frase:")
+frase= input("Crea una frase: ")
 vocales= "aeiou"
 num_vocales=0
 num_consonantes=0
@@ -6,10 +6,9 @@ for letra in frase.lower():
     if letra.isalpha ():
         if letra in vocales:
              num_vocales +=1
-    
-else:
-    num_consonantes +=1 
+        else:
+            num_consonantes +=1 
 
 print("n------Ya esta contado------")
-print(f"en tu frase encontraste {num_vocales} vocales")
-print(f"tambien hay {num_consonantes} consonantes")
+print(f"En tu frase encontraste {num_vocales} vocales")
+print(f"Tambien hay {num_consonantes} consonantes")
